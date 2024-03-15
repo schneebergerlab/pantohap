15b125b0a28f97524bc7272f9064238071ddc44a
mnshgl0110 <mnshgl0110.gmail.com>
2024-03-15 15:39:13 +0100

    Select good unique kmers from windows

--- get_unique_kmer_per_window.sh
+++ get_unique_kmer_per_window.sh
@@ -4,16 +4,22 @@ hap=$2
 cpu=$3
 mem=$4
 meryl=/dss/dsslegfs01/pn29fi/pn29fi-dss-0003/software/bin_manish/anaconda3/envs/mgpy3.8/bin/meryl
+goodkmerdir=/dss/dsslegfs01/pn29fi/pn29fi-dss-0016/projects/potato_hap_example/results/kmer_analysis/
 
+<<comment
 # Get 51-mers in each fasta sequence file
 ls syn_fasta_${genome}_hap${hap}*fasta \
 | sed 's/\.fasta//g' \
 | xargs -n1 -P ${cpu} -I {} $meryl k=51 count threads=1 memory=5 output {} {}.fasta
+comment
+
+# Untar (if tar)
+tar -xf syn_fasta_${genome}_hap${hap}.tar
 
 # Select kmers that are present only once in the fasta
 ls syn_fasta_${genome}_hap${hap}*fasta \
 | sed 's/\.fasta//g' \
-| xargs -n1 -P ${cpu} -I {} bash -c "$meryl print k=51 equal-to 1 threads=1 memory=5 {} > {}.k51.unique.txt " -- {}
+| xargs -n1 -P ${cpu} -I {} bash -c "$meryl print k=51 threads=1 memory=${mem} intersect [ equal-to 1 {} ] ${goodkmerdir}/${genome}_hap$((hap-4))/${genome}_hap$((hap-4))_good  > {}.k51.unique.txt " -- {}
 
 # Cleanup
 tar -cf syn_fasta_${genome}_hap${hap}.tar $(ls -d syn_fasta_${genome}_hap${hap}_bin_*/)
