4d922052cd228789b2d57ba76afab7bfd1e5ff94
mnshgl0110 <mnshgl0110.gmail.com>
2024-05-16 11:50:03 +0200

    Update to run for kmers in (21, 31, 41, 51) for the syn alignments

--- get_unique_kmer_per_window.sh
+++ get_unique_kmer_per_window.sh
@@ -1,30 +1,35 @@
 #!/bin/bash
 genome=$1
 hap=$2
-cpu=$3
-mem=$4
+k=$3
+cpu=$4
+mem=$5
+
 meryl=/dss/dsslegfs01/pn29fi/pn29fi-dss-0003/software/bin_manish/anaconda3/envs/mgpy3.8/bin/meryl
 goodkmerdir=/dss/dsslegfs01/pn29fi/pn29fi-dss-0016/projects/potato_hap_example/results/kmer_analysis/
+synfastadir=/dss/dsslegfs01/pn29fi/pn29fi-dss-0016/projects/potato_hap_example/data/synfastas
 
-# Get 51-mers in each fasta sequence file
-<<comment
+# Extract fasta files from the tars
+tar -xf ${synfastadir}/syn_fasta_${genome}_hap${hap}.fasta.tar.gz
+
+# Get k-mers in each fasta sequence file
 ls syn_fasta_${genome}_hap${hap}*fasta \
 | sed 's/\.fasta//g' \
-| xargs -n1 -P ${cpu} -I {} $meryl k=51 count threads=1 memory=5 output {} {}.fasta
-comment
+| xargs -n1 -P ${cpu} -I {} $meryl k=${k} count threads=1 memory=5 output {} {}.fasta
 
+<<comment
 # Untar (if tar)
 tar -xf syn_fasta_${genome}_hap${hap}.tar
-
-# Select kmers that are present only once in the fasta and are good kmer in the sample
-<<comment
-tar -tf syn_fasta_${genome}_hap${hap}.fasta.tar.gz \
-| sed 's/\.fasta//g' \
-| xargs -n1 -P ${cpu} -I {} bash -c "$meryl k=51 threads=1 memory=${mem} intersect [ equal-to 1 {} ] ${goodkmerdir}/${genome}_hap$((hap-4))/${genome}_hap$((hap-4))_good output {}_good " -- {}
 comment
 
+# Select kmers that are present only once in the fasta and are good kmer in the sample
+ls syn_fasta_${genome}_hap${hap}*fasta \
+| sed 's/\.fasta//g' \
+| xargs -n1 -P ${cpu} -I {} bash -c "$meryl k=51 threads=1 memory=${mem} intersect [ equal-to 1 {} ] ${goodkmerdir}/kmer_size_${k}/${genome}_hap$((hap-4))/${genome}_hap$((hap-4))_good output {}_good " -- {}
+
+
 # Print selected kmers
-tar -tf syn_fasta_${genome}_hap${hap}.fasta.tar.gz \
+ls syn_fasta_${genome}_hap${hap}*fasta \
 | sed 's/\.fasta//g' \
 | xargs -n1 -P ${cpu} -I {} bash -c "$meryl k=51 threads=1 memory=${mem} print {}_good  > {}.k51.good.txt " -- {}
 
@@ -38,9 +43,8 @@ tar -cf syn_fasta_${genome}_hap${hap}.tar $(ls -d syn_fasta_${genome}_hap${hap}_
 rm -r $(ls -d syn_fasta_${genome}_hap${hap}_bin_*/)
 
 # Archive fasta files
-<<comment
 tar -zcf syn_fasta_${genome}_hap${hap}.fasta.tar.gz syn_fasta_${genome}_hap${hap}*fasta
-comment
+rm syn_fasta_${genome}_hap${hap}*fasta
 
 # Archive selected kmers
 ## Currently, not used because util.get_unique_kmers_per_node is set up to require random access to kmer files
--- sbatch_templates/SBATCH_get_unique_kmer_per_window.sh
+++ sbatch_templates/SBATCH_get_unique_kmer_per_window.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
-#SBATCH --array=0-6
+#SBATCH --array=0-9
 #SBATCH --get-user-env
 #SBATCH --clusters=biohpc_gen
 #SBATCH --partition=biohpc_gen_normal
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=10
+#SBATCH --ntasks=4
+#SBATCH --cpus-per-task=4
 #SBATCH --mem-per-cpu=5000mb
 #SBATCH --time=12:00:00
 #SBATCH -J get_unique_kmers
 
-#chars=({A..J})
-chars=( A B C E F I J )
-#for i in 5 6 7 8; do
-for i in 8 ; do
-	srun --exclusive --ntasks 1 --cpus-per-task ${SLURM_CPUS_PER_TASK} --mem-per-cpu=${SLURM_MEM_PER_CPU} \
-	  ../../tool/get_unique_kmer_per_window.sh ${chars[${SLURM_ARRAY_TASK_ID}]} $i 10 5 # threads and mem also parsed
+cwd=/dss/dsslegfs01/pn29fi/pn29fi-dss-0016/projects/potato_hap_example/results/kmer_analysis/
+chars=({A..J})
+
+for i in 5 6 7 8; do
+  {
+  for k in 21 31 41 51; do
+    cd $cwd; cd kmer_size_${k}; cd ${chars[${SLURM_ARRAY_TASK_ID}]}_hap$((hap-4))
+
+    srun --exclusive --ntasks 1 --cpus-per-task ${SLURM_CPUS_PER_TASK} --mem-per-cpu=${SLURM_MEM_PER_CPU} \
+      ../../tool/get_unique_kmer_per_window.sh ${chars[${SLURM_ARRAY_TASK_ID}]} $i $k ${SLURM_CPUS_PER_TASK} 5 # threads and mem also parsed
+  done
+  } &
 done
 wait
