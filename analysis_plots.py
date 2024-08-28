9804b23e828ce8be0958cf4f8d05e46176290a83
mnshgl0110 <mnshgl0110.gmail.com>
2024-08-28 18:03:16 +0200

    Added plots for threading summary

--- analysis_plots.py
+++ analysis_plots.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+
 import matplotlib.pyplot as plt
 
 
@@ -177,6 +179,7 @@ def node_fasta_sequence_stats():
     return
 # END
 
+
 def node_k_stats_plots():
 
     nodestats = pd.read_table('/home/ra98jam/projects/pantohap/results/potato_haps/node_k_stats.txt', header=None)
@@ -319,4 +322,231 @@ def plot_haplotype_graph():
 
     return
 # END
+
+
+def get_haplotype_coverage(gen, mask=None):
+
+    class hapobject:
+        """
+        A haplotype block object
+        """
+
+        def __init__(self, id, start, end, genomes):
+            self.id = id
+            self.start = start
+            self.end = end
+            self.genomes = genomes
+        # END
+
+        def hasgen(self, gen):
+            return gen in self.genomes
+        # END
+
+    # END
+
+    def readthreads(f):
+        listtoint = lambda x: list(map(int, x.replace('[', '').replace(']', '').split(',')))
+        threads = deque()
+        with open(f, 'r') as fin:
+            fin.readline()
+            for line in fin:
+                line = line.strip().split('\t')
+                # ts = listtoint(line[2])
+                ts = listtoint(line[3].replace('imputed_', ''))
+                # Only select threads that cover at least three nodes
+                if len(ts) >= 2:
+                    # threads.append([int(line[1]), listtoint(line[2])])
+                    threads.append([int(line[2]), ts, line[4].split()])
+        return threads
+
+    winlen = 0
+    wincnts = dict(zip(range(5), [0]*5))
+
+    hfin = '/home/ra98jam/d16/projects/potato_hap_example/data/{c}/haplotype_graph_{c}_div0.1_2024_08_02.txt'
+    tfin = '/home/ra98jam/d16/projects/potato_hap_example/results/threading/threads_for_Manish_2024_08_28/{gen}_{c}_div0.1.threads.tsv'
+    chrs = ["chr{:02d}".format(i) for i in range(1, 13)]
+    maskdict = {'chr01': [18115205, 55583177],
+                'chr02': [1, 12410652],
+                'chr03': [6685304, 39750687],
+                'chr04': [13016652, 44345610],
+                'chr05': [13670729, 45907641],
+                'chr06': [6473683, 34511084],
+                'chr07': [7080002, 43669284],
+                'chr08': [8674926, 41322952],
+                'chr09': [9958425, 45185598],
+                'chr10': [5960384, 44289995],
+                'chr11': [16440763, 34686009],
+                'chr12': [9094410, 48036469]}
+
+    for c in chrs:
+        hapfin = hfin.format(c=c)
+        thfin = tfin.format(gen=gen, c=c)
+        # Read haplotype graph
+        hapoblist = deque()
+        with open(hapfin, 'r') as fin:
+            for line in fin:
+                line = line.strip().split()
+                hapoblist.append(hapobject(int(line[3]), int(line[0]), int(line[1]), sorted(line[2].split(','))))
+
+        # Read threads
+        threads = readthreads(thfin)
+        windict = defaultdict(deque)
+        for h in hapoblist:
+            windict[h.start].append(h.id)
+        winfound = defaultdict(deque)
+        for thread in threads:
+            for tnode in thread[1]:
+                winfound[hapoblist[tnode].start].append(tnode)
+        if mask:
+            s = ((maskdict[c][0]//100000)*100000) + 1
+            e = ((maskdict[c][1]//100000)*100000) + 100000 + 1
+            for w in range(s, e, 100000):
+                try:
+                    windict.pop(w)
+                except KeyError:
+                    pass
+                try:
+                    winfound.pop(w)
+                except KeyError:
+                    pass
+        winlen += len(windict)
+        hapcnts = Counter([len(v) for v in winfound.values()])
+        for i in range(5):
+            wincnts[i] += hapcnts[i]
+    haprats = {i: (wincnts[i] if i in wincnts else 0)/winlen for i in range(1, 5)}
+    haprats[0] = 1 - sum(haprats.values())
+    return haprats
+# END
+
+
+def thread_summary_statistics():
+    """
+    Plot Summary statistics for the selected threads
+    """
+    import seaborn as sns
+    import pandas as pd
+    import numpy as np
+    from matplotlib import pyplot as plt
+    from matplotlib import colors as mcol
+    from matplotlib.pyplot import get_cmap
+    from matplotlib.colors import ListedColormap
+    from matplotlib import colormaps
+    from hometools.plot import cleanax
+    import os
+
+    os.chdir('/home/ra98jam/d16/projects/potato_hap_example/results/threading')
+
+    sumdf = pd.read_table('/home/ra98jam/d16/projects/potato_hap_example/results/threading/summary_stats_final/Threading_Summary_Stats_2024_08_24.tsv')
+    sumdf = sumdf[['Sample', 'Pericentromeres', 'Chromosome', 'Thread(2+) Accuracy(%)', 'Thread(2+) Accuracy(%)_noInversions', 'N50','thread(2+) coverage(%)']]
+
+    accdf = sumdf.loc[sumdf['Sample'] == 'White Rose', ['Sample', 'Pericentromeres', 'Chromosome', 'Thread(2+) Accuracy(%)', 'Thread(2+) Accuracy(%)_noInversions']]
+    accdf.columns = ['Sample', 'Pericentromeres', 'Chromosome', 'With_inversion', 'Without_inversion']
+    accdf = accdf.loc[accdf['Sample'] == 'White Rose'].melt(id_vars=['Sample', 'Chromosome', 'Pericentromeres'])
+    accdf['type'] = accdf['Pericentromeres'] + accdf['variable']
+    type_names = {
+        'excludedWith_inversion': 'No_pericentromeres',
+        'includedWith_inversion': 'All_regions',
+        'excludedWithout_inversion': 'No_pericentromeres,No_inversion',
+        'includedWithout_inversion': 'No_inversions'
+    }
+    accdf['type'] = [type_names[t] for t in accdf['type']]
+
+    n50df = sumdf[['Sample', 'Pericentromeres', 'Chromosome', 'N50']]
+    n50df['Pericentromeres'] = ['Chromosome' if t=='included' else 'Chromosome Arm' for t in n50df['Pericentromeres']]
+    covdf = sumdf[['Sample', 'Pericentromeres', 'Chromosome', 'thread(2+) coverage(%)']].copy()
+    covdf['Pericentromeres'] = ['Chromosome' if t=='included' else 'Chromosome Arm' for t in covdf['Pericentromeres']]
+
+
+    # Node calling accuracy
+    fig, ax = plt.subplots(figsize=[4, 4])
+    ax = sns.stripplot(data=accdf, x='type', y='value', order='All_regions No_pericentromeres No_inversions No_pericentromeres,No_inversion'.split(), s=3, ax=ax, color='black')
+    ax = cleanax(ax)
+    ax.set_xlabel('Regions')
+    ax.set_ylabel('Accuracy')
+    ax.tick_params(axis='x', rotation=45)
+    ax.set_ylim(0, 1)
+    plt.tight_layout()
+    plt.savefig('white_rose_node_calling_accuracy.pdf')
+    plt.close()
+
+
+    # N50 Statistics
+    fig, ax = plt.subplots(figsize=[4, 4])
+    ax = sns.stripplot(data=n50df, x='Sample', y='N50', hue='Pericentromeres', dodge=True, ax=ax, hue_order=['Chromosome','Chromosome Arm'], s=3)
+    ax = cleanax(ax)
+    ax.set_xlabel('Genomes')
+    ax.set_ylabel('N50 (in MBp)')
+    ax.legend(frameon=True)
+    ax.tick_params(axis='x', rotation=45)
+    plt.tight_layout()
+    plt.savefig('all_sample_n50s.pdf')
+    plt.close()
+
+
+    # covdf Statistics
+    fig, ax = plt.subplots(figsize=[4, 4])
+    ax = sns.stripplot(data=covdf, x='Sample', y='thread(2+) coverage(%)', hue='Pericentromeres', dodge=True, ax=ax, hue_order=['Chromosome','Chromosome Arm'], s=3)
+    ax = cleanax(ax)
+    ax.set_xlabel('Genomes')
+    ax.set_ylabel('Genome coverage (in %)')
+    ax.legend(frameon=True)
+    ax.tick_params(axis='x', rotation=45)
+    ax.set_ylim(0, 1)
+    plt.tight_layout()
+    plt.savefig('all_sample_genome_coverage.pdf')
+    plt.close()
+
+    # Haplotype Coverage
+    hapstats = dict()
+    for gen in 'WhiteRose Kenva RussetBurbank'.split():
+        for mask in [True, False]:
+            hapstats[(gen, mask)] = get_haplotype_coverage(gen, mask)
+    hapstats = pd.DataFrame(hapstats).T
+    hapstats.reset_index(inplace=True)
+    hapstats = hapstats.melt(id_vars='level_0 level_1'.split())
+    hapstats.columns = 'Sample Pericentromeres variable value'.split()
+    hapstats['Pericentromeres'] = ['Chromosome Arm' if t else 'Chromosome' for t in hapstats['Pericentromeres']]
+
+
+    n_colors = 5
+    # Chromosome colours
+    cmap = colormaps.get_cmap('Reds')
+    discrete_cmap = ListedColormap(cmap(np.linspace(0, 1, n_colors)))
+    chrcolours = dict(zip(range(4, -1, -1), [mcol.to_hex(c) for c in discrete_cmap.colors][::-1]))
+    # Chromosome arm colours
+    cmap = colormaps.get_cmap('Blues')
+    discrete_cmap = ListedColormap(cmap(np.linspace(0, 1, n_colors)))
+    armcolours = dict(zip(range(4, -1, -1), [mcol.to_hex(c) for c in discrete_cmap.colors][::-1]))
+
+
+    fig, ax = plt.subplots(figsize=[4, 4])
+    xticks = 'WhiteRose Kenva RussetBurbank'.split()
+    # axwidth = barwidth*10/pltwidth
+    # axH = fig1.add_axes([0.075, 0.3, axwidth, 0.125])
+    bar_width = 0.75/2
+    hgrp = hapstats.groupby('variable')
+    for i, t in enumerate(['Chromosome', 'Chromosome Arm']):
+        y_bottom = [0]*3
+        for h in range(4, -1, -1):
+            hg = hgrp.get_group(h)
+            hg = hg.loc[hg.Pericentromeres == t]
+            y = [hg.loc[hg.Sample == x, 'value'].tolist()[0] for x in xticks]
+            cols = chrcolours[h] if t == 'Chromosome' else armcolours[h]
+            ecol = chrcolours[4] if t == 'Chromosome' else armcolours[4]
+            ax.bar(np.arange(len(xticks)) + bar_width * (i - 0.5), y, label=f'{h}', bottom=y_bottom, color=cols, edgecolor=ecol, width=bar_width*0.9)
+            y_bottom = [y_bottom[j] + yx for j, yx in enumerate(y)]
+    ax.set_xticks(np.arange(len(xticks)))
+    ax.set_xticklabels(xticks)
+    ax.set_ylabel('Windows (in %)')
+    ax.set_xlabel('')
+    ax = cleanax(ax)
+    ax.set_xlim(-0.5, 2.5)
+    plt.tight_layout()
+    plt.savefig('all_sample_haplotype_coverage.pdf')
+    plt.close()
+
+
+
+# END
+
 
