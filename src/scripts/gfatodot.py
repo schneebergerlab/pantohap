a72fb98b69e9c247f22a5987eccb5b227cf5c187
Leon Rauschning <99650940+lrauschning@users.noreply.github.com>
2025-11-11 16:07:05 +0100

    Fix I/O issue

--- src/scripts/gfatodot.py
+++ src/scripts/gfatodot.py
@@ -39,18 +39,19 @@ def gfa_to_dot(gfa, ):
 if __name__ == "__main__":
     # read in GFA, either from 1st arg or stdin
     try:
-        gfa = Gfa.from_file(sys.argv[1] if (len(sys.argv) > 1 and not sys.argv[1]=='-') else sys.stdin.read(), vlevel=0)
+        #gfa = Gfa.from_file(sys.argv[1] if (len(sys.argv) > 1 and not sys.argv[1]=='-') else "/dev/stdin", vlevel=0) #sys.stdin.read(), vlevel=0)
+        gfa = Gfa.from_file(sys.argv[1], vlevel=0) if (len(sys.argv) > 1 and not sys.argv[1]=='-') else Gfa(sys.stdin.read()[:-1], vlevel=0) # throws very weird errors with the terminating char for some reason
         log(f"Loaded GFA with {len(gfa.segments)} segments and {len(gfa.edges)} edges")
     except Exception as e:
-        log(f"Error loading GFA file: {e}")
+        log(f"Error loading GFA file!")
+        log(e)
         sys.exit(-1)
 
     dot = gfa_to_dot(gfa)
 
     # emit dot
-    if len(sys.argv) > 2 and not sys.argv[2]=='-':
-        print(dot, file=sys.argv[2])
-    else:
-        print(dot)
+    with open(sys.argv[2], "wt") if len(sys.argv) > 2 and not sys.argv[2]=='-' else sys.stdout as f:
+        f.write(dot)
+        f.write("\n")
 
     #TODO have fn to convert into pdf directly?
